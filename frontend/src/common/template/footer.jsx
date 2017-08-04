a9f05016d504976391a10330416d7426b5bd7622
giampaolli <felipegiampaoli@yahoo.com.br>
2017-08-04 15:26:33 -0300

    integrando redux

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2017
+            <a href='https://www.linkedin.com/in/felipe-giampaoli-979b0413a/' target='_blank'> Giampaoli</a>.
+        </strong>
+    </footer>
+)
