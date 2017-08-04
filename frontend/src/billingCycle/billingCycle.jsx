1a04f013c971ce394677e78a0c109a9e088ab8ae
giampaolli <felipegiampaoli@yahoo.com.br>
2017-08-04 17:08:19 -0300

    tabs de cadastro

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,13 +1,36 @@
-import React from  'react'
+import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabsContent from '../common/tab/tabsContent'
+import TabHeader from '../common/tab/tabHeader'
 
-export default props => (
-    <div>
-        <ContentHeader title='Ciclo de Pagamento' small='Versão 1.0'/>
-        <Content>
-            Ciclo de Pagamento
-        </Content>
-    </div>
-)
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars'
+                                target='tabList' />
+                            <TabHeader label='Incluir' icon='plus'
+                                target='tabCreate' />
+                            <TabHeader label='Alterar' icon='pencil'
+                                target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-o'
+                                target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillingCycle
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
 
 import App from './main/app'
 import reducers from './main/reducer'
 
-const store = createStore(reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+
 ReactDOM.render(
         <Provider store={store}>
             <App />
