60261190f8c7bd69dd79d80dd435e4135c1fd248
giampaolli <felipegiampaoli@yahoo.com.br>
2017-08-10 13:03:05 -0300

    finalizando cadastro basico

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-import { getList, showUpdate } from './billingCycleActions'
+import { getList, showUpdate, showDelete } from './billingCycleActions'
 
 class BillingCycleList extends Component {
 
@@ -17,10 +17,19 @@ class BillingCycleList extends Component {
                 <td>{bc.name}</td>
                 <td>{bc.month}</td>
                 <td>{bc.year}</td>
-                <td><button className='btn btn-warning' onClick={() =>
-                    this.props.showUpdate(bc)}>
-                    <i className='fa fa-pencil'></i>
-                </button></td>
+                <td>
+                    <button className='btn btn-warning' onClick={() =>
+                        this.props.showUpdate(bc)}>
+                        <i className='fa fa-pencil'></i>
+                    </button>
+                </td>
+                <td>
+                    <button className='btn btn-danger' onClick={() =>
+                        this.props.showDelete(bc)}>
+                        <i className='fa fa-trash-o'></i>
+                    </button>
+                </td>
+
             </tr>
         ))
     }
@@ -34,7 +43,7 @@ class BillingCycleList extends Component {
                             <th>Nome</th>
                             <th>Mês</th>
                             <th>Ano</th>
-                            <th>Ações</th>
+                            <th className='table-actions'>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -47,5 +56,5 @@ class BillingCycleList extends Component {
 }
 
 const mapStateToPros = state => ({ list: state.billingCycle.list })
-const mapDispatchToProps = dispatch => bindActionCreators({ getList, showUpdate }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ getList, showUpdate, showDelete }, dispatch)
 export default connect(mapStateToPros, mapDispatchToProps)(BillingCycleList)
