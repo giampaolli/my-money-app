0a4a2ec1ca9985d307f13bbfc2f25dcf6a0fa00e
giampaolli <felipegiampaoli@yahoo.com.br>
2017-08-09 23:58:05 -0300

    redux-form

---
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const BASE_URL = 'http://localhost:3004/api'
+
+export function getList() {
+    const request = axios.get(`${BASE_URL}/billingCycles`)
+    return {
+        type: 'BILLING_CYCLES_FETCHED',
+        payload: request
+    }
+}
+
+export function create(values) {
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+                dispatch([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList', 'tabCreate')
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro',
+                    error))
+            })
+    }
+}
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,13 +4,15 @@ import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 import App from './main/app'
 import reducers from './main/reducer'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
   && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
         <Provider store={store}>
